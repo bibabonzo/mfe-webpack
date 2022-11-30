bac541de0218ac0809442871439b6e8038fae1c1
Joachim Wiemes <joachim.wiemes@web.de>
2022-11-30 13:52:06 +0100

    Trigger auth deployment...

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -8,10 +8,10 @@ import SignUp from "./components/Signup";
 const generateClassName = createGenerateClassName({
   productionPrefix: 'au'
 })
-export default ({history}) => {
+export default ({ history }) => {
   return <div>
         <StylesProvider generateClassName={generateClassName} >
-          <Router history={history}>
+          <Router history={ history }>
             <Switch>
                 <Route path="/auth/signin" component={SignIn} />
                 <Route path="/auth/signup" component={SignUp} />
