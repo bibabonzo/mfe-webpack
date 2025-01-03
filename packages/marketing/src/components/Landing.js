bbaf8a64c992e2d3f0f6ce5221db236cfff4438d
Joachim Wiemes <joachim.wiemes@web.de>
2025-01-03 14:23:59 +0100

    Fixed random image loading issue.

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -129,7 +129,7 @@ export default function Album() {
                 <Card className={classes.card}>
                   <CardMedia
                     className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
+                    image="https://picsum.photos/800/600"
                     title="Image title"
                   />
                   <CardContent className={classes.cardContent}>
